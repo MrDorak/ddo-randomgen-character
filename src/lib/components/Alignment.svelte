ff2ba69835ecb3d96b2cabe4608854f7d80c7893
Dorian <d.turle@rhinos.fr>
2023-06-22 15:15:56 +0200

    fix(alignment): fix alignment not checking

--- src/lib/components/Alignment.svelte
+++ src/lib/components/Alignment.svelte
@@ -5,8 +5,7 @@
         fetchStore,
         hasAllLawfulAlignmentsSelected,
         hasAllNeutralAlignmentsSelected,
-        hasAllChaoticAlignmentsSelected,
-        selectedStartingStats
+        hasAllChaoticAlignmentsSelected
     } from '../../store'
     import { writable } from "svelte/store";
 
@@ -65,9 +64,11 @@
                 <div class="flex flex-wrap justify-center gap-3 p-2 grow rounded-lg text-gray-900 bg-gray-100 dark:bg-gray-700 dark:text-white">
                     {#each Object.values($data.alignments) as data}
                         <div class="flex items-center pl-3">
-                            <input id="alignments_{data.name}" type="checkbox" bind:checked={data.selected} value="{data.name}" on:click={handleChange}
+                            <input id="alignments_{data.name}" type="checkbox" bind:checked={data.selected} on:change={handleChange}
                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                            <label for="alignments_{data.name}" class="w-full ml-2 text-sm font-medium">{data.name}</label>
+                            <label for="alignments_{data.name}" class="w-full ml-2 text-sm font-medium">
+                                {data.name}
+                            </label>
                         </div>
                     {/each}
                 </div>
